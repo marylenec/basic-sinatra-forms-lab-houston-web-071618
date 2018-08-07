290d05a0f049db31b6139b1596031eb9c8c0f208
Marylene Chan <marylenechan@Marylenes-MacBook-Pro-2.local>
2018-08-06 23:22:11 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,22 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  # Add your post route and action below
+  # action here
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+
+    erb :team
+  end
 
 end
